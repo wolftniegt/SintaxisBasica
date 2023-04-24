04c25757b7cd383af6e4340820e4fe90755d36ec
Liliana Herrera <33354745+luzlyherrera@users.noreply.github.com>
2023-04-24 10:56:48 -0500

    Sintaxis básica en JavaScript

---
+++ 3-constantes.js
@@ -0,0 +1,10 @@
+/*Constantes.
+Son datos que no cambian en la ejecución del código, se definen de la siguiente manera.
+    const NOMBRE_CONSTANTE = valorConstante
+Por ejemplo, en mátematicas el valor de PI no cambia, es cosntante. 
+Vea el siguiente código*/
+
+const PI=3.1416
+/*Interpolación de variables: Tener dentro de una cadena de texto un valor dinamico de la siguiente manera
+    `Texto ${variable / constante}`*/
+console.log(`El valor de PI es: ${PI}`)
